0ae7b2ed4d60ff83548e368e22225d45acdea953
Daniel Ugochukwu <slade8546@gmail.com>
2026-06-18 10:03:51 +0100

    Clean up Navbar component imports and add VSCode settings

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,13 +4,7 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import {
-  Show,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-  useUser,
-} from "@clerk/nextjs";
+import { Show, SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 
 const navItems = [
